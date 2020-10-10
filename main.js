be46621bcb9bf8f7fc97cdd1f16fbe539b849d5e
renee-coder <71918527+renee-coder@users.noreply.github.com>
2020-10-10 12:48:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,5 +11,25 @@ recognition.onresult=function(event){
     console.log(event);
     var Content = event.results[0][0].transcript;
     console.log(Content);
+
+    speak();
+
     document.getElementById("textbox").innerHTML=Content;
 }
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera); 
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    img_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
