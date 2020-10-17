3628c938505911963ae7174d049ba04692bdafc3
renee-coder <71918527+renee-coder@users.noreply.github.com>
2020-10-17 12:40:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,17 +12,24 @@ recognition.onresult=function(event){
     var Content = event.results[0][0].transcript;
     console.log(Content);
 
-    speak();
+    if(Content=="take my selfie"){
+        console.log("taking selfie in 5 seconds");
+        speak();
+    }
 
     document.getElementById("textbox").innerHTML=Content;
 }
 
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "taking your selfie in 5 seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera); 
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
 }
 
 Webcam.set({
@@ -33,3 +40,16 @@ Webcam.set({
 });
 
 camera=document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
+}
